d721a175b115a1230e5bae4127a3183e7b030e9d
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-23 09:21:51 -0300

    Create project :ghost:

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// obtém o elemento do filtro
+// obtém os elementos das cartas
+
+// adiciona evento de entrada para o elemento de filtro
+
+// função de filtro
+   // se o filtro não estiver vazio
+     // para cada carta de cartas
+       // obtém o título do cartão (título)
+       // transforma para minúsculas
+       // transforma o texto do filtro em minúsculas
+       // se o título do cartão não incluir o texto do filtro
+         // esconde o elemento card
+       // senão
+         // exibe o elemento do cartão
+   // senão
+     // para cada carta de cartas
+       // exibe o elemento do cartão
