2d13cf755b87a75586af673e3f15e2a44330e9be
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-23 11:43:07 -0300

    Update :heavy_check_mark:

--- js/script.js
+++ js/script.js
@@ -1,21 +1,40 @@
 // obtém o elemento do filtro
+const filterElement = document.querySelector("header input");
+
 // obtém os elementos das cartas
+const cards = document.querySelectorAll(".cards li");
 
 // adiciona evento de entrada para o elemento de filtro
+filterElement.addEventListener("input", filterCards);
 
 // função de filtro
-   // se o filtro não estiver vazio
-     // para cada carta de cartas
-       // obtém o título do cartão (título)
-       // transforma para minúsculas
-       // transforma o texto do filtro em minúsculas
-       // se o título do cartão não incluir o texto do filtro
-         // esconde o elemento card
-       // senão
-         // exibe o elemento do cartão
-   // senão
-     // para cada carta de cartas
-       // exibe o elemento do cartão
+function filterCards() {
+  // se o filtro não estiver vazio
+  if ((filterElement.textContent = !"")) {
+    // para cada carta de cartas
+    for (let card of cards) {
+      // obtém o título do cartão (título)
+      let title = card.querySelector("h2");
 
+      // transforma para minúsculas
+      title = title.textContent.toLowerCase();
 
-/* ======= ======= */
+      // transforma o texto do filtro em minúsculas
+      let filterText = filterElement.value.toLowerCase();
+      // se o título do cartão não incluir o texto do filtro
+      if (!title.includes(filterText)) {
+        // esconde o elemento card
+        card.style.display = "none";
+      } else {
+        // exibe o elemento do cartão
+        card.style.display = "block";
+      }
+    }
+  } else {
+    // para cada carta de cartas
+    for(let card of cards){
+    // exibe o elemento do cartão
+        card.style.display = "block";
+    }
+  }
+}
